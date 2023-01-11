6a7d54889b01907d3cb84f8a52c7e1bac228d184
Tahir Khan <77186729+tahhirkhan@users.noreply.github.com>
2023-01-11 15:57:11 +0530

    update

--- Arrays/Sorting/bubbleSort.java
+++ Arrays/Sorting/bubbleSort.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {6, 5, 3, 7, 1, 0, 2, 4};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void sort(int[] arr) {
+        boolean swapped = false;
         for (int i=0; i<arr.length; i++) {
             for (int j=1; j<arr.length; j++) {
                 if (arr[j] < arr[j-1]) {
@@ -16,7 +17,11 @@ public class BubbleSort {
                     int temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
+            }
+            if (!swapped) {
+                break;
             }
         }
     }
