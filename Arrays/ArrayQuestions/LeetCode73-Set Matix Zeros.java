a3c089cba95d0eb198467dac6c0d831133b7f00f
Tahir Khan <77186729+tahhirkhan@users.noreply.github.com>
2023-01-28 19:24:23 +0530

    Create file

---
+++ Arrays/ArrayQuestions/LeetCode73-Set Matix Zeros.java
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/set-matrix-zeroes/description/
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[0].length; j++) {
+                if (row.contains(i) || col.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
