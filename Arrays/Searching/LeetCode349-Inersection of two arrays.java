6a48ff3abf5f2bcf396fd9844b2a6f07abea1d67
Tahir Khan <77186729+tahhirkhan@users.noreply.github.com>
2023-03-25 12:53:12 +0530

    Create file

---
+++ Arrays/Searching/LeetCode349-Inersection of two arrays.java
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/intersection-of-two-arrays/
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for (int i=0; i<nums1.length; i++) {
+            set1.add(nums1[i]);
+        }
+
+        for (int i=0; i<nums2.length; i++) {
+            if (set1.contains(nums2[i])) {
+                set2.add(nums2[i]);
+            }
+        }
+
+        int[] ans = new int[set2.size()];
+        int i = 0;
+        for (int s : set2) {
+            ans[i] = s;
+            i++;
+        }
+
+        return ans;
+    }
+}
