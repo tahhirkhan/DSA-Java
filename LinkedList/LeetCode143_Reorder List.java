c5182f4640454d5f042370e7070d00b6c831d2ba
Tahir Khan <77186729+tahhirkhan@users.noreply.github.com>
2023-07-13 12:06:35 +0530

    Update file

--- LinkedList/LeetCode143_Reorder List.java
+++ LinkedList/LeetCode143_Reorder List.java
@@ -1,13 +1,13 @@
 https://leetcode.com/problems/reorder-list/description/
 
-public class Solution {
-	public static Node rearrangeList(Node head) {
-		Node mid = middleNode(head);
-		Node h1 = head;
-		Node h2 = reverse(mid);
+class Solution {
+	public void reorderList(ListNode head) {
+		ListNode mid = middleNode(head);
+		ListNode h1 = head;
+		ListNode h2 = reverse(mid);
 		while (h1 != null && h2 != null) {
-			Node temp1 = h1.next;
-			Node temp2 = h2.next;
+			ListNode temp1 = h1.next;
+			ListNode temp2 = h2.next;
 			h1.next = h2;
 			h2.next = temp1;
 			h1 = temp1;
@@ -16,22 +16,21 @@ public class Solution {
 		if (h1 != null) {
 			h1.next = null;
 		}
-		return head;
 	}
-	public static Node middleNode(Node head) {
-		Node fast = head;
-		Node slow = head;
+	public static ListNode middleNode(ListNode head) {
+		ListNode fast = head;
+		ListNode slow = head;
 		while (fast != null && fast.next != null) {
 			slow = slow.next;
 			fast = fast.next.next;
 		}
 		return slow;
 	}
-	public static Node reverse(Node node) {
-		Node prev = null;
-		Node curr = node;
+	public static ListNode reverse(ListNode node) {
+		ListNode prev = null;
+		ListNode curr = node;
 		while (curr != null) {
-			Node next = curr.next;
+			ListNode next = curr.next;
 			curr.next = prev;
 			prev = curr;
 			curr = next;
@@ -39,57 +38,3 @@ public class Solution {
 		return prev;
 	}
 }
-
-
-
-
-
-
-
-// class Solution {
-//     public void reorderList(ListNode head) {
-//         ListNode mid = midNode(head);
-
-//         // Reverse the second half of the list:
-//         ListNode prev = null;
-//         ListNode curr = mid;
-//         while (curr != null) {
-//             ListNode next = curr.next;
-//             curr.next = prev;
-//             prev = curr;
-//             curr = next;
-//         }
-//         // Now:
-//         ListNode h1 = head;
-//         ListNode h2 = prev;
-//         while (h1 != null && h2 != null) {
-//             // ListNode temp1 = h1.next;
-//             // h1.next = h2;
-//             // h1 = temp1;
-
-//             // temp1 = h2.next;
-//             // h2.next = h1;
-//             // h2 = temp1;
-
-//             ListNode temp1 = h1.next;
-//             ListNode temp2 = h2.next;
-//             h1.next = h2;
-//             h2.next = temp1;
-//             h1 = temp1;
-//             h2 = temp2;
-//         }
-//         if (h1 != null) {
-//             h1.next = null;
-//         }
-//     }
-//     // Find middle node of a linked list:
-//     private ListNode midNode(ListNode head) {
-//         ListNode fast = head;
-//         ListNode slow = head;
-//         while (fast != null && fast.next != null) {
-//             fast = fast.next.next;
-//             slow = slow.next;
-//         }
-//         return slow;
-//     }
-// }
