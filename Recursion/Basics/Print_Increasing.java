3f7cf84bb4891c94dac4934253362f1f847b20e9
Tahir Khan <77186729+tahhirkhan@users.noreply.github.com>
2023-07-16 21:01:06 +0530

    Create file

---
+++ Recursion/Basics/Print_Increasing.java
@@ -0,0 +1,14 @@
+package Recursion.Basics;
+
+public class PrintIncreasing {
+    public static void main(String[] args) {
+        printIncreasing(5);
+    }
+    public static void printIncreasing(int n) {
+        if (n == 0) {
+            return;
+        }
+        printIncreasing(n-1);
+        System.out.print(n+" ");
+    }
+}
