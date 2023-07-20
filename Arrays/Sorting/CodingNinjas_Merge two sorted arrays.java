d2cd684d7d11e34f9d77bc2e6bf5ececfdb14656
Tahir Khan <77186729+tahhirkhan@users.noreply.github.com>
2023-07-20 16:43:03 +0530

    Create file

---
+++ Arrays/Sorting/CodingNinjas_Merge two sorted arrays.java
@@ -0,0 +1,26 @@
+https://www.codingninjas.com/studio/problems/ninja-and-sorted-arrays_1214628?leftPanelTab=0
+
+public class Solution {
+    public static int[] ninjaAndSortedArrays(int arr1[], int arr2[], int m, int n) {
+        // Write your code here.
+        int x = m - 1;
+        int y = n - 1;
+        int last = m + n - 1;
+        while (y >= 0 ) {
+            if (x >= 0 && arr1[x] >= arr2[y]) {
+                arr1[last] = arr1[x];
+                x--;
+            } else {
+                arr1[last] = arr2[y];
+                y--;
+            }
+            last--;
+
+        }
+        // if (x < 0) {
+        //     arr1[last] = arr2[y];
+        // }
+
+        return arr1;
+    }
+}
